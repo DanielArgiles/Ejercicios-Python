7162f4a41e7f3b2bcf78aa325641770d1c95a38e
Daniel Argilés <59224533+DanielArgiles@users.noreply.github.com>
2020-05-02 14:54:58 +0200

    Update regresion_logistica_spark.py

--- regresion_logistica_spark.py
+++ regresion_logistica_spark.py
@@ -111,3 +111,82 @@ AUC= my_eval.evaluate(results)
 # Obtengo un valor de AUC (Area Under the Curve)=0,81
 # AUC se utiliza en el análisis de clasificación para determinar cuál de los modelos utilizados predice mejor las clases. 
 AUC
+
+#------#
+# Proyecto de consultoría basado en Regresión Logística (clasificación)
+# ¡Una importante agencia de marketing de Nueva York se ha puesto en contacto con usted para ayudarlos con predicción de la rotación de clientes!
+# La rotación de clientes es el porcentaje de clientes que dejaron de usar el producto o servicio de su empresa durante un período de tiempo determinado. 
+# Puede calcular la tasa de abandono dividiendo la cantidad de clientes que perdió durante ese período de tiempo, digamos un trimestre, por la cantidad de clientes que tenía al comienzo de ese período.
+# Una agencia de marketing tiene muchos clientes que utilizan su servicio para producir anuncios para los sitios web de clientes .
+# Se han dado cuenta de que tienen bastante rotación en los clientes.
+# Actualmente asignan al azar administradores de cuenta, pero quieren que cree un modelo de aprendizaje automático que ayude a predecir qué clientes abandonarán (dejarán de comprar su servicio) para que puedan asignar correctamente a los clientes con mayor riesgo de abandonar un administrador de cuentas.
+# La compañía puede probar esto con los datos entrantes para futuros clientes para predecir qué clientes abandonarán y asignarles un administrador de cuentas.
+# Datos en customer_churn.csv
+# Su objetivo es crear un modelo (algoritmo de clasificación) que pueda predecir si un cliente abandonará (0 o 1) en función de las características.
+#¡Recuerde que el administrador de la cuenta está actualmente asignado aleatoriamente!
+"""
+Name : Name of the latest contact at Company
+Age: Customer Age
+Total_Purchase: Total Ads Purchased
+Account_Manager: Binary 0=No manager, 1= Account manager assigned
+Years: Total Years as a customer
+Num_sites: Number of websites that use the service.
+Onboard_date: Date that the name of the latest contact was onboarded
+Location: Client HQ Address
+Company: Name of Client Company
+Churn: 0 or 1 indicating whether customer has churned.
+"""
+  
+from pyspark.sql import SparkSession
+spark= SparkSession.builder.appName('logregconsult').getOrCreate()
+from pyspark.ml.classification import LogisticRegression
+data=spark.read.csv("customer_churn.csv",inferSchema=True,header=True)
+data.printSchema()
+# Chequeamos los datos
+data.describe().show()
+data.columns
+# Formato para MLlib
+from pyspark.ml.feature import VectorAssembler
+# Seleccionamos las características útiles de entrada, es decir, las columnas numéricas
+assembler= VectorAssembler(inputCols=['Age',
+ 'Total_Purchase',
+ 'Account_Manager',
+ 'Years',
+ 'Num_Sites'],outputCol='features')
+# Transformamos nuestros datos
+output=assembler.transform(data)
+# churn o rotación de clientes es lo que queremos predecir
+final_data=output.select('features','churn')
+# Test Train Split
+train_churn,test_churn=final_data.randomSplit([0.7,0.3])
+from pyspark.ml.classification import LogisticRegression
+# Creamos el modelo
+lr_churn= LogisticRegression(labelCol='churn')
+# Ajustamos este modelo a los datos
+fitted_churn_model=lr_churn.fit(train_churn)
+# Verificamos un resumen
+training_sum=fitted_churn_model.summary
+training_sum.predictions.describe().show()
+# Evaluamos los resultados en el conjunto de datos que nos dieron (usando los datos de prueba)
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+pred_and_labels=fitted_churn_model.evaluate(test_churn)
+pred_and_labels.predictions.show()
+churn_eval=BinaryClassificationEvaluator(rawPredictionCol='prediction',
+                                        labelCol='churn')
+auc=churn_eval.evaluate(pred_and_labels.predictions)
+# Obtengo un valor de auc= 0,73, valor bueno
+# Un valor de 0.5 será tan bueno como adivinar al azar
+auc
+# Ahora intentamos predecir sobre datos nuevos sin etiquetar
+final_lr_model=lr_churn.fit(final_data)
+new_customers=spark.read.csv('new_customers.csv',inferSchema=True,
+                            header=True)
+new_customers.printSchema()
+test_new_customers=assembler.transform(new_customers)
+test_new_customers.printSchema()
+final_results= final_lr_model.transform(test_new_customers)
+# Predicciones de los 6 clientes para la empresa
+final_results.select('Company','prediction').show()
+# Vemos que solo hay 6 clientes
+test_new_customers.describe().show()
+# ¡Ahora sabemos que debemos asignar administradores de cuenta a Cannon-Benson, Barron-Robertson, Sexton-GOlden y Parks-Robbins, ya que obtienen un 1 de predicción !
