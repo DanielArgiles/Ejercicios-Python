0d17f87654e0d54c666357f3315133ee543f4848
Daniel Argilés <59224533+DanielArgiles@users.noreply.github.com>
2020-05-20 13:17:05 +0200

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -283,8 +283,218 @@ result.show(truncate=False)
 +---+---------------+-------------------------+
 """
 
-#-----NLP Code Along-----#
+#-----NLP Code Along: Filtro de detección de spam-----#
+# Nuestro conjunto de datos consta de mensajes de texto voluntarios de un estudio en Singapur y algunos mensajes de spam de un sitio de informes del Reino Unido.
 
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('nlp').getOrCreate()
+data= spark.read.csv('smsspamcollection/SMSSpamCollection',inferSchema=True,sep='\t')
+data.show()
+"""
++----+--------------------+
+| _c0|                 _c1|
++----+--------------------+
+| ham|Go until jurong p...|
+| ham|Ok lar... Joking ...|
+|spam|Free entry in 2 a...|
+| ham|U dun say so earl...|
+| ham|Nah I don't think...|
+|spam|FreeMsg Hey there...|
+| ham|Even my brother i...|
+| ham|As per your reque...|
+|spam|WINNER!! As a val...|
+|spam|Had your mobile 1...|
+| ham|I'm gonna be home...|
+|spam|SIX chances to wi...|
+|spam|URGENT! You have ...|
+| ham|I've been searchi...|
+| ham|I HAVE A DATE ON ...|
+|spam|XXXMobileMovieClu...|
+| ham|Oh k...i'm watchi...|
+| ham|Eh u remember how...|
+| ham|Fine if thats th...|
+|spam|England v Macedon...|
++----+--------------------+
+only showing top 20 rows
+"""
+
+data= data.withColumnRenamed('_c0','class').withColumnRenamed('_c1','text')
+data.show()
+"""
++-----+--------------------+
+|class|                text|
++-----+--------------------+
+|  ham|Go until jurong p...|
+|  ham|Ok lar... Joking ...|
+| spam|Free entry in 2 a...|
+|  ham|U dun say so earl...|
+|  ham|Nah I don't think...|
+| spam|FreeMsg Hey there...|
+|  ham|Even my brother i...|
+|  ham|As per your reque...|
+| spam|WINNER!! As a val...|
+| spam|Had your mobile 1...|
+|  ham|I'm gonna be home...|
+| spam|SIX chances to wi...|
+| spam|URGENT! You have ...|
+|  ham|I've been searchi...|
+|  ham|I HAVE A DATE ON ...|
+| spam|XXXMobileMovieClu...|
+|  ham|Oh k...i'm watchi...|
+|  ham|Eh u remember how...|
+|  ham|Fine if thats th...|
+| spam|England v Macedon...|
++-----+--------------------+
+only showing top 20 rows
+"""
+
+from pyspark.sql.functions import length
+data= data.withColumn('length',length(data['text']))
+data.show()
+"""
++-----+--------------------+------+
+|class|                text|length|
++-----+--------------------+------+
+|  ham|Go until jurong p...|   111|
+|  ham|Ok lar... Joking ...|    29|
+| spam|Free entry in 2 a...|   155|
+|  ham|U dun say so earl...|    49|
+|  ham|Nah I don't think...|    61|
+| spam|FreeMsg Hey there...|   147|
+|  ham|Even my brother i...|    77|
+|  ham|As per your reque...|   160|
+| spam|WINNER!! As a val...|   157|
+| spam|Had your mobile 1...|   154|
+|  ham|I'm gonna be home...|   109|
+| spam|SIX chances to wi...|   136|
+| spam|URGENT! You have ...|   155|
+|  ham|I've been searchi...|   196|
+|  ham|I HAVE A DATE ON ...|    35|
+| spam|XXXMobileMovieClu...|   149|
+|  ham|Oh k...i'm watchi...|    26|
+|  ham|Eh u remember how...|    81|
+|  ham|Fine if thats th...|    56|
+| spam|England v Macedon...|   155|
++-----+--------------------+------+
+only showing top 20 rows
+"""
+data.groupBy('class').mean().show()
+"""
++-----+-----------------+
+|class|      avg(length)|
++-----+-----------------+
+|  ham|71.45431945307645|
+| spam|138.6706827309237|
++-----+-----------------+
+"""
+
+from pyspark.ml.feature import (Tokenizer, StopWordsRemover, CountVectorizer,
+                               IDF, StringIndexer)
+tokenizer= Tokenizer(inputCol='text',outputCol='token_text')
+stop_remove=StopWordsRemover(inputCol='token_text',outputCol='stop_token')
+count_vec= CountVectorizer(inputCol='stop_token',outputCol='c_vec')
+idf=IDF(inputCol='c_vec',outputCol='tf_idf')
+ham_spam_to_numeric= StringIndexer(inputCol='class',outputCol='label')
+
+from pyspark.ml.feature import VectorAssembler
+clean_up= VectorAssembler(inputCols=['tf_idf','length'],outputCol='features')
+
+from pyspark.ml.classification import NaiveBayes
+nb= NaiveBayes()
+
+from pyspark.ml import Pipeline
+data_prep_pipe= Pipeline(stages=[ham_spam_to_numeric,tokenizer,
+                                stop_remove,count_vec,idf,clean_up])
+cleaner=data_prep_pipe.fit(data)
+clean_data=cleaner.transform(data)
+clean_data.columns
+"""
+['class',
+ 'text',
+ 'length',
+ 'label',
+ 'token_text',
+ 'stop_token',
+ 'c_vec',
+ 'tf_idf',
+ 'features']
+"""
+
+clean_data=clean_data.select('label','features')
+clean_data.show()
+"""
++-----+--------------------+
+|label|            features|
++-----+--------------------+
+|  0.0|(13424,[7,11,31,6...|
+|  0.0|(13424,[0,24,297,...|
+|  1.0|(13424,[2,13,19,3...|
+|  0.0|(13424,[0,70,80,1...|
+|  0.0|(13424,[36,134,31...|
+|  1.0|(13424,[10,60,139...|
+|  0.0|(13424,[10,53,103...|
+|  0.0|(13424,[125,184,4...|
+|  1.0|(13424,[1,47,118,...|
+|  1.0|(13424,[0,1,13,27...|
+|  0.0|(13424,[18,43,120...|
+|  1.0|(13424,[8,17,37,8...|
+|  1.0|(13424,[13,30,47,...|
+|  0.0|(13424,[39,96,217...|
+|  0.0|(13424,[552,1697,...|
+|  1.0|(13424,[30,109,11...|
+|  0.0|(13424,[82,214,47...|
+|  0.0|(13424,[0,2,49,13...|
+|  0.0|(13424,[0,74,105,...|
+|  1.0|(13424,[4,30,33,5...|
++-----+--------------------+
+only showing top 20 rows
+"""
+training,test=clean_data.randomSplit([0.7,0.3])
+spam_detector=nb.fit(training)
+data.printSchema()
+"""
+root
+ |-- class: string (nullable = true)
+ |-- text: string (nullable = true)
+ |-- length: integer (nullable = true)
+"""
+test_results= spam_detector.transform(test)
+test_results.show()
+"""
++-----+--------------------+--------------------+--------------------+----------+
+|label|            features|       rawPrediction|         probability|prediction|
++-----+--------------------+--------------------+--------------------+----------+
+|  0.0|(13424,[0,1,4,50,...|[-830.19229371859...|[1.0,7.1865697329...|       0.0|
+|  0.0|(13424,[0,1,7,15,...|[-656.19608717291...|[1.0,3.7522464876...|       0.0|
+|  0.0|(13424,[0,1,9,14,...|[-539.18915845895...|[1.0,9.5065245384...|       0.0|
+|  0.0|(13424,[0,1,12,33...|[-454.53625070496...|[1.0,1.2828752326...|       0.0|
+|  0.0|(13424,[0,1,14,18...|[-1366.2835003483...|[1.0,5.9348099662...|       0.0|
+|  0.0|(13424,[0,1,15,20...|[-684.53383879069...|[1.0,5.1261818260...|       0.0|
+|  0.0|(13424,[0,1,20,27...|[-967.78516925213...|[1.0,1.2918828208...|       0.0|
+|  0.0|(13424,[0,1,21,27...|[-760.33177790758...|[1.0,1.7839552118...|       0.0|
+|  0.0|(13424,[0,1,23,63...|[-1294.6486382999...|[1.0,3.1061933096...|       0.0|
+|  0.0|(13424,[0,1,24,31...|[-355.73202891079...|[1.0,9.0190240887...|       0.0|
+|  0.0|(13424,[0,1,27,35...|[-1485.9667899645...|[0.99999997455451...|       0.0|
+|  0.0|(13424,[0,1,43,69...|[-616.31896474537...|[0.99998808877209...|       0.0|
+|  0.0|(13424,[0,1,416,6...|[-300.05701948277...|[0.99999999999999...|       0.0|
+|  0.0|(13424,[0,2,3,5,6...|[-2572.2209931774...|[1.0,2.0649701650...|       0.0|
+|  0.0|(13424,[0,2,3,6,9...|[-3295.7663122378...|[1.0,9.63149943E-...|       0.0|
+|  0.0|(13424,[0,2,4,5,7...|[-995.44841272981...|[1.0,3.3751903107...|       0.0|
+|  0.0|(13424,[0,2,4,7,2...|[-512.67288900125...|[1.0,4.1013386632...|       0.0|
+|  0.0|(13424,[0,2,4,8,2...|[-568.69782770176...|[1.0,2.4576072052...|       0.0|
+|  0.0|(13424,[0,2,4,10,...|[-1219.4411622787...|[1.0,9.8986542758...|       0.0|
+|  0.0|(13424,[0,2,4,40,...|[-1580.3026844172...|[0.99999999999993...|       0.0|
++-----+--------------------+--------------------+--------------------+----------+
+only showing top 20 rows
+"""
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+acc_eval=MulticlassClassificationEvaluator()
+acc=acc_eval.evaluate(test_results)
+print('ACC of NB Model')
+print(acc) # Obtengo 0.9296950535275138 que es un 92 % de precisión
+
+# Ahora podemos probar a cambiar por otro modelo : from pyspark.ml.classification import NaiveBayes
+# Y ver si la precisión del modelo mejora
 
 
 
